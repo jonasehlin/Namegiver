d8568e4ce61f67845be2cb13bd699829e39afc90
Jonas Ehlin <jonas.ehlin@joliv.se>
2018-12-03 22:08:00 +0100

    Add entity Name

---
+++ Namegiver/Models/Name.cs
@@ -0,0 +1,10 @@
+﻿namespace Namegiver.Models
+{
+	public class Name
+	{
+		public int Id { get; set; }
+		public string Text { get; set; }
+		public bool Accepted { get; set; }
+		public int RejectCount { get; set; }
+	}
+}
