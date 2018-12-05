a15d35151120efb6c403a68c7276d99b25c4479d
Jonas Ehlin <jonas.ehlin@joliv.se>
2018-12-05 11:35:19 +0100

    Create function updateRandomName

--- Namegiver/wwwroot/js/site.ts
+++ Namegiver/wwwroot/js/site.ts
@@ -1,19 +1,18 @@
 ﻿module Namegiver {
+	var nameId = 0;
+
+	interface Name {
+		Id: number;
+		Text: string;
+		Accepted: boolean;
+		RejectedCount: number;
+		Language: string;
+		Gender: number;
+	}
+
 	$(document).ready(function () {
 
-		var nameId = 0;
-
-		$.ajax({
-			url: '/api/names',
-			type: 'GET'
-		}).done(function (data, status, xhr) {
-			console.log('Success, data', data);
-			$('#name').text(data.text);
-			nameId = data.id;
-		}).fail(function (jqXhr, textStatus, errorMessage) {
-			console.log('Error in response from /api/names', textStatus);
-			nameId = -1;
-		});
+		updateRandomName();
 
 		$('#accept').click(function () {
 			if (nameId > 0) {
@@ -22,8 +21,8 @@
 					url: '/api/names/' + nameId + '/accept',
 					type: 'PUT'
 				}).done(function (data, status, xhr) {
-					alert('Accept: ' + nameId);
-					// TODO: Load new random name
+					alert('Name "' + + '" accepted!');
+					updateRandomName();
 				}).fail(function (jqXhr, textStatus, errorMessage) {
 					alert('Error: ' + nameId);
 				});
@@ -37,12 +36,25 @@
 					url: '/api/names/' + nameId + '/reject',
 					type: 'PUT'
 				}).done(function (data, status, xhr) {
-					alert('Reject: ' + nameId);
-					// TODO: Load new random name
+					updateRandomName();
 				}).fail(function (jqXhr, textStatus, errorMessage) {
 					alert('Error: ' + nameId);
 				});
 			}
 		});
 	});
+
+	function updateRandomName() {
+		$.ajax({
+			url: '/api/names',
+			type: 'GET'
+		}).done(function (data, status, xhr) {
+			console.log('Success, data', data);
+			$('#name').text(data.text);
+			nameId = data.id;
+		}).fail(function (jqXhr, textStatus, errorMessage) {
+			console.log('Error in response from /api/names', textStatus);
+			nameId = -1;
+		});
+	}
 }
