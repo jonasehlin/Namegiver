ebbb75fbba9b2bb01ea9e1bfdf9705de41e4b1f1
Jonas Ehlin <jonas.ehlin@joliv.se>
2019-01-11 13:37:01 +0100

    Add top rejected names page

---
+++ Namegiver/Pages/TopRejectedNames.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Namegiver.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Namegiver.Pages
+{
+	public class TopRejectedNamesModel : PageModel
+	{
+		public IEnumerable<Name> TopList { get; private set; }
+
+		private readonly IConfiguration Configuration;
+
+		public TopRejectedNamesModel(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public async Task OnGetAsync()
+		{
+			using (var db = NamegiverContext.CreateDefault(Configuration))
+			{
+				TopList = await db.Names.GetTopRejectedNames();
+			}
+		}
+	}
+}
