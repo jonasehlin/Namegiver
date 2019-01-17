26716a3d2fb9ae009e3440b26df4f87d1cf2e547
Jonas Ehlin <jonas.ehlin@joliv.se>
2019-01-18 00:07:47 +0100

    Add Namegiver database sql script

---
+++ Namegiver/Scripts/NamegiverDb.sql
@@ -0,0 +1,27 @@
+﻿/*
+	Creates or updates a Namegiver database.
+*/
+
+CREATE TABLE [dbo].[Name] (
+    [Id]        INT     IDENTITY (1, 1) NOT NULL,
+    [Gender]    TINYINT NULL,
+    [SuperType] TINYINT NULL,
+    CONSTRAINT [PK_NameId] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+GO
+
+CREATE TABLE [dbo].[NameInfo] (
+    [Id]            INT           IDENTITY (1, 1) NOT NULL,
+    [NameId]        INT           NOT NULL,
+    [Name]          NVARCHAR (20) NOT NULL,
+    [Accepted]      BIT           CONSTRAINT [DF_Name_Accepted] DEFAULT ((0)) NOT NULL,
+    [RejectedCount] INT           CONSTRAINT [DF_Name_RejectCount] DEFAULT ((0)) NOT NULL,
+    [Language]      NVARCHAR (5)  NULL,
+    CONSTRAINT [PK_NameInfo] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_NameInfo_NameId] FOREIGN KEY ([NameId]) REFERENCES [dbo].[Name] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [IX_Name_Unique]
+    ON [dbo].[NameInfo]([Name] ASC);
+GO
