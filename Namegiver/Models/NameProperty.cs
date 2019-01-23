b692c79829abd4ccdf208ff6fa1cd57f33e784f0
Jonas Ehlin <jonas.ehlin@joliv.se>
2019-01-23 23:16:07 +0100

    Update models

---
+++ Namegiver/Models/NameProperty.cs
@@ -0,0 +1,10 @@
+﻿namespace Namegiver.Models
+{
+	public class NameProperty
+	{
+		public int Id { get; set; }
+		public int NameId { get; set; }
+		public string Key { get; set; }
+		public string Value { get; set; }
+	}
+}
